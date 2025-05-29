fed36577b1d0672ad3739f055ac0da998800a3bd
Kshithij01 <kshithijraik@gmail.com>
2025-05-29 11:31:52 +0530

    first commit

---
+++ src/main/java/lab8/prg8/App.java
@@ -0,0 +1,13 @@
+package lab8.prg8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
